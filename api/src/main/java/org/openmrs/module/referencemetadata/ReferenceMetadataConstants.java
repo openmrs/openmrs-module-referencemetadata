e99dc5c5b13d04985c4329872a508fabc9e65900
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2014-02-05 10:19:53 +0100

    RA-282 Make unknown location to be visit, transfer and admission location

--- api/src/main/java/org/openmrs/module/referencemetadata/ReferenceMetadataConstants.java
+++ api/src/main/java/org/openmrs/module/referencemetadata/ReferenceMetadataConstants.java
@@ -13,5 +13,13 @@ public class ReferenceMetadataConstants {
     public final static String OPENMRS_ID_GENERATOR_NAME = "Generator for " + OPENMRS_ID_NAME;
 
     public final static String UNKNOWN_LOCATION_UUID = "8d6c993e-c2cc-11de-8d13-0010c6dffd0f";
+    
+    public static final String VISIT_LOCATION_TAG_UUID = "37dd4458-dc9e-4ae6-a1f1-789c1162d37b";
+	
+	public static final String ADMISSION_LOCATION_TAG_UUID = "1c783dca-fd54-4ea8-a0fc-2875374e9cb6";
+	
+	public static final String TRANSFER_LOCATION_TAG_UUID = "8d4626ca-7abd-42ad-be48-56767bbcf272";
+	
+	public static final String LOGIN_LOCATION_TAG_UUID = "b8bbf83e-645f-451f-8efe-a0db56f09676";
 
 }
