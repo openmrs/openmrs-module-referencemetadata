0f844d724c9f4b020f9a151cb2e81a3ff08bb0fd
Rafal Korytkowski <rafal.korytkowski@gmail.com>
2015-03-23 21:12:15 +0100

    Fixed test

--- api/src/test/java/org/openmrs/module/referencemetadata/ReferenceMetadataActivatorComponentTest.java
+++ api/src/test/java/org/openmrs/module/referencemetadata/ReferenceMetadataActivatorComponentTest.java
@@ -127,7 +127,7 @@ public class ReferenceMetadataActivatorComponentTest extends BaseModuleContextSe
         assertThat(conceptService.getConcept(5085).getUuid(), is("5085AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
         assertThat(conceptService.getConcept(159947).getUuid(), is("159947AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
         
-        assertThat(conceptService.getAllConcepts().size(), is(417));
+        assertThat(conceptService.getAllConcepts().size(), is(423));
     }
 
 }
