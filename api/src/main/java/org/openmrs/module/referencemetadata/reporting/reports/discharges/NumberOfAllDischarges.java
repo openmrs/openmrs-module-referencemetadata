e9a86a234d9125024394550aaff256a62db91e43
Sumangala028 <sumangala028@gmail.com>
2018-06-25 21:23:08 +0530

    Added Number of All Discharges report and Changed package name according to the grouping
    Minor fix
    
    Minor fix

---
+++ api/src/main/java/org/openmrs/module/referencemetadata/reporting/reports/discharges/NumberOfAllDischarges.java
@@ -0,0 +1,99 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.referencemetadata.reporting.reports.discharges;
+
+import org.openmrs.module.reporting.dataset.definition.SqlDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.openmrs.module.reporting.report.manager.BaseReportManager;
+import org.openmrs.module.reporting.report.manager.ReportManagerUtil;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class NumberOfAllDischarges extends BaseReportManager {
+
+    private static final String DATA_SET_UUID = "f03c2413-3f8b-4bf8-93ec-6d48ce11a6e1";
+
+    public NumberOfAllDischarges() {
+    }
+
+    @Override
+    public String getUuid() {
+        return "d1a00e10-cecb-4775-8c4f-2ed0b059d7b9";
+    }
+
+    @Override
+    public String getName() {
+        return "Number of All Discharges";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Number of All Discharges for a given location";
+    }
+
+    @Override
+    public List<Parameter> getParameters() {
+        List<Parameter> parameterArrayList = new ArrayList<Parameter>();
+        return parameterArrayList;
+    }
+
+    @Override
+    public ReportDefinition constructReportDefinition() {
+        ReportDefinition reportDef = new ReportDefinition();
+        reportDef.setUuid(getUuid());
+        reportDef.setName(getName());
+        reportDef.setDescription(getDescription());
+        reportDef.setParameters(getParameters());
+
+        SqlDataSetDefinition sqlDataDef = new SqlDataSetDefinition();
+        sqlDataDef.setUuid(DATA_SET_UUID);
+        sqlDataDef.setName(getName());
+        sqlDataDef.addParameters(getParameters());
+        sqlDataDef.setSqlQuery(getSQLQuery());
+
+        reportDef.addDataSetDefinition("All Discharges Count", Mapped.mapStraightThrough(sqlDataDef));
+
+
+        return reportDef;
+    }
+
+    @Override
+    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+        List<ReportDesign> l = new ArrayList<ReportDesign>();
+        l.add(ReportManagerUtil.createExcelDesign("8c795d47-992f-4d4d-9f92-9f3dae39b52b", reportDefinition));
+        return l;
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+
+    private String getSQLQuery(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("select  max(loc.name) as Location, count(*) as Count from encounter e  ");
+        stringBuilder.append("inner join location loc on loc.location_id=e.location_id ");
+        stringBuilder.append("where e.encounter_type=(select encounter_type_id from encounter_type where uuid='181820aa-88c9-479b-9077-af92f5364329') ");
+        stringBuilder.append("and e.voided = 0 ");
+        stringBuilder.append("group by e.location_id ");
+
+        return stringBuilder.toString();
+    }
+}
---
+++ api/src/test/java/org/openmrs/module/referencemetadata/reports/discharges/NumberOfDischargesTest.java
@@ -0,0 +1,47 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.referencemetadata.reports.discharges;
+
+import org.junit.Assert;
+import org.openmrs.Location;
+import org.openmrs.module.referencemetadata.reporting.reports.discharges.NumberOfDischarges;
+import org.openmrs.module.referencemetadata.reports.ReportManagerTest;
+import org.openmrs.module.reporting.dataset.SimpleDataSet;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.report.ReportData;
+import org.openmrs.module.reporting.report.manager.ReportManager;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.hamcrest.Matchers.contains;
+
+public class NumberOfDischargesTest extends ReportManagerTest {
+
+	@Autowired
+	NumberOfDischarges numberOfDischarges;
+
+	public ReportManager getReportManager() {
+		return numberOfDischarges;
+	}
+
+	public EvaluationContext getEvaluationContext() {
+		EvaluationContext context = new EvaluationContext();
+		context.addParameterValue("location", new Location(Location.LOCATION_UNKNOWN));
+		return context;
+	}
+
+	public void verifyData(ReportData data) {
+		SimpleDataSet dataSet = (SimpleDataSet) data.getDataSets().values().iterator().next();
+		Assert.assertThat(dataSet.getRows(), contains(hasData("Count", 2L)));
+	}
+}
