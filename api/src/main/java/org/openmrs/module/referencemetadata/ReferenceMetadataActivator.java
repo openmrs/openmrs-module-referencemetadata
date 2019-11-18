186bd8c25964387867ed9c7809949ea881e70165
mozzy11 <mozzymutesa@gmail.com>
2019-11-18 12:43:56 +0300

    RA-1634 : Place Numeric Concepts in Appropriate packages

--- api/src/main/java/org/openmrs/module/referencemetadata/ReferenceMetadataActivator.java
+++ api/src/main/java/org/openmrs/module/referencemetadata/ReferenceMetadataActivator.java
@@ -96,10 +96,10 @@ public class ReferenceMetadataActivator extends BaseModuleActivator {
             dataImporter.importData("Reference_Application_Concepts-25.xml");
             
             if (ModuleUtil.compareVersion(OpenmrsConstants.OPENMRS_VERSION_SHORT, "2.2") < 0) {
-            	dataImporter.importData("Reference_Application_Numeric_Concepts-24-pre2.x.xml");
+            	dataImporter.importData("Reference_Application_Numeric_Concepts-25-pre2.x.xml");
 			}
             else {
-            	dataImporter.importData("Reference_Application_Numeric_Concepts-24-2.x.xml");
+            	dataImporter.importData("Reference_Application_Numeric_Concepts-25-2.x.xml");
             }
             
             dataImporter.importData("Reference_Application_Diagnoses-12.xml");
